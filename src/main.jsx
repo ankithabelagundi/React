01d4c44c1165a49ab4b374f5ca003b7965fdda89
Ankitha R B <ankithabelagundi@gmail.com>
2025-12-16 14:26:16 +0530

    Multi-components

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-=======
-import MainPage from './components/MainPage.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <MainPage/>
->>>>>>> master
   </StrictMode>,
 )
